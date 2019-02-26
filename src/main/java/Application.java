f9e68927042ad1791354891cf53c7bf54aef6049
leonardofmoura <up201706907@fe.up.pt>
2019-02-26 11:43:17 +0000

    First test using Lanterna

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,27 @@
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import sun.awt.X11.Screen;
+
+import javax.smartcardio.TerminalFactory;
+import java.io.IOException;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        try {
+            Terminal terminal =new DefaultTerminalFactory().createTerminal();
+            TerminalScreen screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null); //we dont need a cursor
+            screen.startScreen();           //screens must be started
+            screen.doResizeIfNecessary();   //resize screen if necessary
+
+            screen.clear();
+            screen.setCharacter(10,10, new TextCharacter('X'));
+            screen.refresh();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
