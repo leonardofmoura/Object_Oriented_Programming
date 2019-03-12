35c46502e213fd72a738176b2e9468d1f41de3a1
leonardofmoura <up201706907@fe.up.pt>
2019-03-12 10:54:12 +0000

    Modified the repo to include all the course

---
+++ TP1/src/Application.java
@@ -0,0 +1,6 @@
+public class Application {
+    public static void main(String args[]) {
+        System.out.println("Thank you for using the Hello World app");
+        System.out.print("Version 1.0");
+    }
+}
